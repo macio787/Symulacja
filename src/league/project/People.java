8558287fc88d3c0c8460c024f5e0742bf4ef33c2
macio787 <maciek.z.haciek@gmail.com>
2019-04-16 17:47:42 +0200

    trzeci commit, po branchui

--- src/league/project/People.java
+++ src/league/project/People.java
@@ -5,13 +5,21 @@ public class People implements IPeople {
     private int power;
     private ITeam team;
 
+    @Override
     public int getPower(){
         return this.power;
     };
+
+    @Override
     public ITeam getTeam(){
         return this.team;
-    };
-    public void setTeam(ITeam team){
+    }
+
+    @Override
+    public void setTeam(ITeam team) {
         this.team=team;
-    };
+    }
+
+
+    ;
 }
