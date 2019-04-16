09d2fec3e6f451811f61912b98081307d223210e
macio787 <maciek.z.haciek@gmail.com>
2019-04-16 18:15:38 +0200

    zmienione nazwy w Player.java

--- src/league/project/Player.java
+++ src/league/project/Player.java
@@ -1,29 +1,32 @@
 package league.project;
 
+import league.project.ITeam;
+import league.project.People;
+
 import java.util.Random;
-public class Player extends People{
+public class Player extends People {
     //chcemy raczej generować imiona i nazwiska ale możliwość dodania pojedynczych na +
     private String name;
     //popraw na pełne nazwy poniżej
-    private int pac;
-    private int sho;
-    private int def;
-    private int pas;
-    private int dri;
-    private int phy;
+    private int pace;
+    private int shoot;
+    private int defence;
+    private int passing;
+    private int drible;
+    private int physics;
     private int power;
     private ITeam team;
 
     public Player(String name){
         Random rand = new Random();
         this.name = name;
-        this.pac = rand.nextInt(99)+1;
-        this.sho = rand.nextInt(99)+1;
-        this.def = rand.nextInt(99)+1;
-        this.pas = rand.nextInt(99)+1;
-        this.dri = rand.nextInt(99)+1;
-        this.phy = rand.nextInt(99)+1;
-        this.power = (pac+sho+def+pas+dri+phy)/6;
+        this.pace = rand.nextInt(99)+1;
+        this.shoot = rand.nextInt(99)+1;
+        this.defence = rand.nextInt(99)+1;
+        this.passing = rand.nextInt(99)+1;
+        this.drible = rand.nextInt(99)+1;
+        this.physics = rand.nextInt(99)+1;
+        this.power = (pace+shoot+defence+passing+drible+physics)/6;
     }
 
 }
