f91cd55e097ad2216ea455884968c8509e42ce9b
Szymon Draszkiewicz <SzymonDraszkiewicz9@gmail.com>
2019-04-16 13:11:11 +0200

    fix - zdublowany interfejs

--- src/league/project/ITeam.java
+++ src/league/project/ITeam.java
@@ -1,13 +1,7 @@
 package league.project;
 
-<<<<<<< HEAD
 public interface ITeam {
-
-}
-=======
-    public interface ITeam {
         int getPower();
         void setPower(int x);
         int getPoints();
-    }
->>>>>>> dffa2bca9de7a18aeecbcbc3320d4716ed2594d9
+}
